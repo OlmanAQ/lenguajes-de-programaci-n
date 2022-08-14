01036c5c9c22e45f76e8674b3f984c554c44aaa2
Olman Alonso Acuña Quesada <61434202+OlmanAQ@users.noreply.github.com>
2022-08-13 20:19:43 -0600

    Terminado

--- Paradigma Imperativo/E_1/main.go
+++ Paradigma Imperativo/E_1/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	text := "Hola mundo\nen lenguaje GO"
+
+	fmt.Println("\nEl string: ", text)
+
+	caracteres := len(text)
+
+	saltos := strings.Count(text, "\n")
+
+	var palabras int
+	campos := strings.Fields(text)
+	palabras = len(campos)
+
+	fmt.Println("\nSaltos de linea: ", saltos)
+	fmt.Println("Caracteres: ", caracteres)
+	fmt.Println("Palabras: ", palabras)
+
+}
