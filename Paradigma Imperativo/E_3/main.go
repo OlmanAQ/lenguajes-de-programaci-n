5eb4cd5632df06018515db616a52abd640039ef2
Olman Alonso Acuña Quesada <61434202+OlmanAQ@users.noreply.github.com>
2022-08-18 19:57:33 -0600

    termiinado

--- Paradigma Imperativo/E_3/main.go
+++ Paradigma Imperativo/E_3/main.go
@@ -4,24 +4,47 @@ import (
 	"fmt"
 )
 
-func rotarArreglo(arr *[8]string, dirreccion string, rotaciones int) {
+func rotarArreglo(arr *[8]string, direccion string, rotar int) [8]string {
 
-	for i := range arr {
+	for rot := 0; rot < rotar; rot++ {
+		str1 := "Derecha"
+		result := str1 == direccion
+		if result == true {
+			var aux string = arr[7]
+			for i := len(arr) - 1; i > -1; i-- {
+				if i != 0 {
+					arr[i] = arr[i-1]
+				} else {
+					arr[0] = aux
+				}
 
+			}
+
+		} else {
+			var aux string = arr[0]
+			for i := 0; i < len(arr); i++ {
+				if i != 7 {
+					arr[i] = arr[i+1]
+				} else {
+					arr[7] = aux
+				}
+			}
+		}
 	}
-
+	return *arr
 }
 
 func main() {
 
 	arr := [8]string{"a", "b", "c", "d", "e", "f", "g", "h"}
 	dirreccion := "Derecha"
-	var rotar = 5
+	var rotar = 3
 
 	fmt.Println("Arreglo: ", arr)
 	fmt.Println("Direccion: ", dirreccion)
 	fmt.Println("Rotacciones: ", rotar)
 
-	rotarArreglo(&arr, dirreccion, rotar)
+	var newarr = rotarArreglo(&arr, dirreccion, rotar)
+	fmt.Println("Arreglo rotado: ", newarr)
 
 }
